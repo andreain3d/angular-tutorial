7a1689b48e9b8909938a1cbacb6d2dabe7e71dfd
Andrea N <andreain3d@gmail.com>
2022-01-24 08:53:15 -0500

    added basic components, recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 // import { ServerComponent } from './practice/server/server.component';
 // import { ServersComponent } from './practice/servers/servers.component';
 // import { WarningAlertComponent } from './practice/warning-alert/warning-alert.component';
@@ -14,6 +21,13 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
     // ServerComponent,
     // ServersComponent,
     // WarningAlertComponent,
